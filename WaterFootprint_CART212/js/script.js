e46fef62736f253019182c8392bf0700c541095e
Angel CC <angelcenerini@gmail.com>
2021-04-08 10:39:40 -0400

    WF: Setting Up Active State

--- WaterFootprint_CART212/js/script.js
+++ WaterFootprint_CART212/js/script.js
@@ -8,8 +8,10 @@ CART212 Final Project
 */
 
 let myFont = undefined;
+let lights = undefined;
+let avatar = undefined;
 
-let state = `title` // Title, Animation, Attive, Passive
+let state = `attive` // Title, Animation, Attive, Passive
 
 /**
 Description of preload
@@ -17,6 +19,9 @@ Description of preload
 function preload() {
 
   myFont = loadFont('assets/pixelated.otf');
+
+  lights = loadImage('assets/images/lightGIF6.gif');
+  avatar = loadImage('assets/images/clown.png');
 
 }
 
@@ -26,10 +31,12 @@ Description of setup
 */
 function setup() {
 
-  createCanvas(720, 520);
+  createCanvas(850, 520);
   noStroke();
   textFont(myFont);
   textAlign(RIGHT, RIGHT);
+  rectMode(CENTER);
+  imageMode(CENTER);
 }
 
 
@@ -37,7 +44,7 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(35);
+  background(30);
 
   if (state === `title`){
 
@@ -52,12 +59,23 @@ function draw() {
     text(`Press  SPACEBAR  to  start`, width/2, 440);
     pop();
 
-
   }
   else if (state === `animation`){
 
   }
   else if (state === `attive`){
+
+    // User Platform
+    push();
+    fill(139, 217, 199);
+    rect(150, 220, 80, 20);
+    pop();
+
+    // Avatar
+    image(avatar, 150, 170);
+
+    // Lights
+    image(lights, 670, height/2);
 
   }
   else if (state === `passive`){
